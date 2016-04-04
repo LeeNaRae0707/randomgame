1fc571bba6832d64c15a596fc6f2df6b01fd09b0
Narae Lee <duk4957@naver.com>
2016-04-04 20:14:49 +0900

    #7 add image resouces

--- app/src/main/java/kr/hs/duk4957_emirim/adventuretime/MainActivity.java
+++ app/src/main/java/kr/hs/duk4957_emirim/adventuretime/MainActivity.java
@@ -2,19 +2,23 @@ package kr.hs.duk4957_emirim.adventuretime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mName;
-
+    Button mButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mName=(EditText) findViewById(R.id.name);
-
+        mButton =(Button) findViewById(R.id.btn_show_me_the_char);
+        mButton.setOnClickListener(this);
     }
 
     @Override
@@ -22,4 +26,10 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
         mName.setText("");
     }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "싸워요!",Toast.LENGTH_LONG).show();
+    }
 }
+
--- app/src/main/java/kr/hs/duk4957_emirim/adventuretime/ResultActivity.java
+++ app/src/main/java/kr/hs/duk4957_emirim/adventuretime/ResultActivity.java
@@ -3,15 +3,20 @@ package kr.hs.duk4957_emirim.adventuretime;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by appcreate22 on 2016. 3. 28..
  */
 public class ResultActivity extends AppCompatActivity {
 
+    TextView mResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mResult =(TextView)findViewById(R.id.result);
+        mResult.setText("결과");
     }
 }
